1d43814e14f806fb5fb54ebeac4926887ee57c39
Sattar Feizollahibarough <sattar@sattar.sic.net>
2020-04-10 11:49:40 +0430

    connect to MySQL

--- mysqlDB/mysqlDB.go
+++ mysqlDB/mysqlDB.go
@@ -1,16 +1,29 @@
-package mysqlDB 
+package mysqlDB
 
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"strings"
 )
 
-func Initialize(username string, password string, hostname string, port string,dbname string) *sql.DB {
-	connectionString := username +":"+ password + "@tcp(" + hostname + ":" + port + ")/"+dbname
+func Initialize(username string, password string, hostname string, port string, dbname string) *sql.DB {
+	connectionString := username + ":" + password + "@tcp(" + hostname + ":" + port + ")/" + dbname
 	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		panic(err.Error())
 	}
-        return db;   
+	return db
 
+}
+func CreateTable(db *sql.DB, tablename string, columns map[string]string) {
+	query := "CREATE TABLE IF NOT EXISTS " + tablename + "("
+	for colname, coltype := range columns {
+		query += colname + " " + coltype + ","
+	}
+		lastindx:=strings.LastIndex(query, ",")
+	query = query[:lastindx]+strings.Replace(query[lastindx:], ",", ")", 1)
+	_, err := db.Query(query)
+	if err != nil {
+		panic(err.Error())
+	}
 }
